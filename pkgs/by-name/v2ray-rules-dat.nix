3f81bd156bf8f19cf19bde6f583bcdc3c8448fb4
qbisi <84267544+qbisi@users.noreply.github.com>
2026-06-02 03:39:50 +0000

    [gha] Nix: update packages

--- pkgs/by-name/v2ray-rules-dat.nix
+++ pkgs/by-name/v2ray-rules-dat.nix
@@ -7,7 +7,7 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "v2ray-rules-dat";
-  version = "202605312255";
+  version = "202606012322";
 
   src1 = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${finalAttrs.version}/geoip.dat";
@@ -16,7 +16,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   src2 = fetchurl {
     url = "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/${finalAttrs.version}/geosite.dat";
-    hash = "sha256-jkamnglQDZKfyAizLEtkiXVIFR5HaXqmIzfbnVl+6sM=";
+    hash = "sha256-toikU4DU4G4CQ/agLWnFwfQozmQDj18i/JaUENWS7cU=";
   };
 
   dontUnpack = true;
